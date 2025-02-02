e463fdee6cefa05d52b24d1a713cabde5a1f4935
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-02-03 02:19:48 +0400

    defining user model

--- workout_app/users/models.py
+++ workout_app/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import EmailValidator
 
-# Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True, validators=[EmailValidator()])
+    password = models.CharField(max_length=255, )
+    username = None
+
+    USERNAME_FIELD = None
+    REQUIRED_FIELDS = []
