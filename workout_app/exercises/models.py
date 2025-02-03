4bf523a44eea668a3c59baaf9e19f96dcb782cce
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-02-04 01:34:09 +0400

    defining workoutplan, personalized exercises and running migrations

--- workout_app/exercises/models.py
+++ workout_app/exercises/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from users.models import User
+from django.core.validators import MaxLengthValidator, MinValueValidator
+from django.utils import timezone
 
 class Exercises(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -12,3 +15,42 @@ class Exercises(models.Model):
 
     def __str__(self):
         return self.name
+    
+class WorkoutPlan(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, unique=True)
+    goal = models.CharField(max_length=255, unique=True)
+    workout_frequency = models. IntegerField(
+        help_text="Times per week",
+        validators=[MinValueValidator(1), MaxLengthValidator(7)]
+    )
+    session_duration = models.IntegerField(
+        help_text="Duration in minutes. Daily.",
+        validators=[MinValueValidator(5), MaxLengthValidator(1440)]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+class PersonalizedExercise(models.Model):
+    workout_plan = models.ForeignKey(WorkoutPlan, on_delete=models.CASCADE, related_name='exercises')
+    exercise = models.ForeignKey(Exercises, on_delete=models.CASCADE)
+    sets = models.IntegerField(
+        null=True, blank=True,
+        validators=[MinValueValidator(0)]
+        )
+    repetitions = models.IntegerField(
+        null=True, blank=True,
+        validators=[MinValueValidator(0)]
+    )
+    duration = models.FloatField(
+        help_text='duration in hours',
+        default=0,
+        validators=[MinValueValidator(0)]
+    )
+    distance = models.FloatField(
+        help_text="Distance in kilometers.",
+        null = True, blank=True,
+        validators=[MinValueValidator(0)]
+    )
