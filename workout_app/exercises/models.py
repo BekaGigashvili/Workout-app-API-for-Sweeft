4c5fcf5d8053bdb709805ccb0002f15253135efc
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-02-04 00:05:50 +0400

    defining exercises model

--- workout_app/exercises/models.py
+++ workout_app/exercises/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Exercises(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    instructions = models.TextField()
+    target_muscles = models.CharField(max_length=200)
+    equipment = models.CharField(max_length=100, blank=True, null=True)
+    difficulty_level = models.CharField(max_length=100)
+    distance_related = models.BooleanField(default=False)
+    sets_and_repetitions_related = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
